f3126ed3e96064d774f46bc36d6b2e2ea63d7acc
Malvin95 <Malvinharding@hotmail.co.uk>
2016-12-13 19:04:04 +0000

    Update Volume.java

--- CourseWork/src/corpus/Volume.java
+++ CourseWork/src/corpus/Volume.java
@@ -14,4 +14,32 @@ public class Volume {
 	public Volume() {
 		chapters = new ArrayList<Chapter>();
 	}
+	
+	public void addChapter(Chapter ch)
+	{
+		chapters.add(ch);
+	}
+	
+	/*
+	 * A method that iterates through the Paragraphs array and check if 
+	 * a Paragraph exists, based on this it returns a boolean.
+	 */
+	public boolean Search(int p)
+	{
+		boolean exists = false;
+		
+		for(int i = 0; i < chapters.size(); i++)
+		{
+			if(chapters.indexOf(i) == p)
+			{
+				exists = true;
+				return exists;
+			}
+		}
+		
+		exists = false;
+		return exists;
+	}
+	
+	
 }
