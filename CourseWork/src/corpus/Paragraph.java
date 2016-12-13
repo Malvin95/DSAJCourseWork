c1173111205e5cfbc8759373c92e42e93cf2488d
Malvin95 <Malvinharding@hotmail.co.uk>
2016-12-13 19:03:11 +0000

    Update Paragraph.java

--- CourseWork/src/corpus/Paragraph.java
+++ CourseWork/src/corpus/Paragraph.java
@@ -1,17 +1,65 @@
 package corpus;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+
 /**
  * A class which models the makeup of a Paragraph.
  * 
- * @author S Saddique
+ * @author S Saddique, M Harding
  * @version 08/12/2016
  *
  */
+
 public class Paragraph {
-	private HashMap<String, Word> words;
 	
-	public Paragraph() {
-		words = new HashMap<String, Word>();
+	private ArrayList<Word> words;
+	
+	public Paragraph()
+	{
+		words = new ArrayList<Word>();
 	}
+	
+	public void addWord(Word word)
+	{
+		words.add(word);
+	}
+	
+	/*
+	 * A search function that tests to see if a word exists within a paragraph
+	 */
+	
+	public int Search(String s)
+	{
+		int j = 0;
+		for(int i = 0; i < words.size(); i++)
+		{
+			if(s.equals(words.get(i).getWord()))
+			{
+				j++;
+			}
+		}
+		return j;
+	}
+	
+	/*
+	 * A Search function that searches for every appearance of a word object 
+	 * and adds it to an array that lists info about said word.
+	 * The system would then print out the Index of the word 
+	 * as well as the Word itself.
+	 */
+	
+	public void everyAppearance(String s)
+	{
+		ArrayList<Word> ea = new ArrayList<Word>();
+		
+		for(int i = 0; i < words.size(); i++)
+		{
+			if(s.equals(words.get(i).getWord()))
+			{
+				ea.add(words.get(i));
+				System.out.println("Word Index:" + i + words.get(i).getWord());
+			}
+		}
+	}
+	
 }
