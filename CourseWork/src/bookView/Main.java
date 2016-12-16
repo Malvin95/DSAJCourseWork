e4331a2459f9752f15b30c17a88597c9ff51f2d2
saddiqs1 <ssaddique96@hotmail.com>
2016-12-16 00:27:16 +0000

    Update Main.java

--- CourseWork/src/bookView/Main.java
+++ CourseWork/src/bookView/Main.java
@@ -1,8 +1,10 @@
 package bookView;
 
+import java.io.IOException;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		MainController mc = new MainController();
 		TUI tui = new TUI(mc);
 	}
